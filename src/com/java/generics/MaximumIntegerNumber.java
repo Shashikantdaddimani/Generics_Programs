adabc1d6885ae9f7660ab7807a32036195d9908c
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-07 14:13:25 +0530

    Added:Logic for display the message of generics program

---
+++ src/com/java/generics/MaximumIntegerNumber.java
@@ -0,0 +1,11 @@
+package com.java.generics;
+/**
+ * Class to create MaximumIntegerNumber with different methods
+ */
+public class MaximumIntegerNumber {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Genarics Program");
+	
+	}
+}
